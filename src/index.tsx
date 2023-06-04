25f28b1a570fb15fd0943f583c21a83052706328
kor_ka <badgrib@gmail.com>
2023-06-04 06:45:18 +0400

    fix(client): try fix early button

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import { SessionModel } from "./model/SessionModel";
 import reportWebVitals from "./reportWebVitals";
 
-
 const tryInit = () => {
   const wa = (window as any).Telegram?.WebApp
   if (!wa) {
@@ -26,25 +25,28 @@ const tryInit = () => {
 
 
   import('./view/MainScreen').then(({ AddExpenceScreen, AddTransferScreen, MainScreen, ModelContext, UserContext, UsersProvider }) => {
-    const router = createBrowserRouter([
-      {
-        path: "/tg",
-        element: <MainScreen />,
-      },
-      {
-        path: "/tg/addExpence",
-        element: <AddExpenceScreen />,
-      },
-      {
-        path: "/tg/addPayment",
-        element: <AddTransferScreen />,
-      },
-    ]);
 
     const sub = model.balance.subscribe((b) => {
       if (b) {
         sub();
         wa.MainButton.offClick(goToAddExpense)
+
+        // initing router late so it handle possible path change by early button
+        const router = createBrowserRouter([
+          {
+            path: "/tg",
+            element: <MainScreen />,
+          },
+          {
+            path: "/tg/addExpence",
+            element: <AddExpenceScreen />,
+          },
+          {
+            path: "/tg/addPayment",
+            element: <AddTransferScreen />,
+          },
+        ]);
+
         root.render(
           <ModelContext.Provider value={model}>
             <UserContext.Provider value={model.tgWebApp.user.id}>
