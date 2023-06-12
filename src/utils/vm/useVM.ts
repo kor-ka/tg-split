3fa9d11bdc8f70ee93b4122086cb6f4319320ab6
kor_ka <badgrib@gmail.com>
2023-06-12 23:49:17 +0400

    fix(client): useVMvalue hook fix

--- src/utils/vm/useVM.ts
+++ src/utils/vm/useVM.ts
@@ -3,7 +3,7 @@ import { VM } from "./VM";
 export function useVMvalue<T>(vm: VM<T>) {
   let [val, setValue] = React.useState(vm.val);
   React.useEffect(() => {
-    return vm.subscribe(v => setValue(v));
-  }, []);
+    return vm.subscribe(setValue);
+  }, [vm]);
   return val;
 }
