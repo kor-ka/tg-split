ae5122314afaa1acefa0df3428d691bf194771ee
Gleb Putintsev <badgrib@gmail.com>
2023-06-15 17:25:07 +0400

    core(client): format sum K optional

--- src/view/utils/formatSum.ts
+++ src/view/utils/formatSum.ts
@@ -1,7 +1,7 @@
-export const formatSum = (sum: number, abs = true) => {
+export const formatSum = (sum: number, abs = true, k = true) => {
     let s = ((abs ? Math.abs(sum) : sum) / 100)
 
-    if (s >= 1000 && ((s / 100) % 1 === 0)) {
+    if (k && (s >= 1000 && ((s / 100) % 1 === 0))) {
         s /= 1000
         return s + 'K'
     }
