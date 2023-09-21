d487968b1c19e547ea6250bd3fcfd529a7c50841
Gleb Putintsev <badgrib@gmail.com>
2023-09-21 23:39:59 +0400

    feat(tg): /stat group button

--- server/src/api/tg/tg.ts
+++ server/src/api/tg/tg.ts
@@ -152,10 +152,14 @@ And don't forget to pin the message with the button, so everyone can open the ap
       }
     });
 
-    this.bot.onText(/\/start/, async (upd) => {
+    this.bot.onText(/\/start$/, async (upd) => {
       try {
-        await this.chatMetaModule.updateChat(upd.chat.id, upd.chat.title ?? "");
-        await this.createPin(upd.chat.id, upd.message_thread_id);
+        await this.bot.sendMessage(
+            upd.chat.id,
+            'Hey👋\nThis bot is meant to work in groups with your friends, add me to any group to start.',
+            { reply_markup: { inline_keyboard: [[{ text: 'Add to group', url: "https://telegram.me/splitsimplebot?startgroup=true" }]] } }
+          );
+
       } catch (e) {
         console.log(e);
       }
